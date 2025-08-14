72993f010d568e7bbb4f920d7bf32cf3a5f6c020
Alfred Ryvarden <alfredryvarden@gmail.com>
2025-08-14 09:04:50 +0200

    Init commit

---
+++ src/main/java/com/booleanuk/api/Main.java
@@ -0,0 +1,4 @@
+package com.booleanuk.api;
+
+public class Main {
+}
---
+++ src/test/java/com/booleanuk/simplejokeapi/SimplejokeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booleanuk.simplejokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplejokeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
